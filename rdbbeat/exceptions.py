1ddb193fd1c606eafe91b453902f45da7074c90f
evanstjabadi <evansltjabadi@gmail.com>
2023-08-21 22:10:18 +0200

    add the notice

--- rdbbeat/exceptions.py
+++ rdbbeat/exceptions.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2023 Hewlett Packard Enterprise Development LP
+# MIT License
+
+
 class PeriodicTaskNotFound(Exception):
     """
     Raised when a periodic task is not found in the database.
